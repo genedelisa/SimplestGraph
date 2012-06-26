d216fe471a50b2a03f9e3cf1310da02b2d8b6692
Gene De Lisa <gene@rockhoppertech.com>
2012-06-26 07:11:38 -0400

    Initial Commit

---
+++ SimplestGraphTests/SimplestGraphTests.h
@@ -0,0 +1,13 @@
+//
+//  SimplestGraphTests.h
+//  SimplestGraphTests
+//
+//  Created by Gene De Lisa on 6/26/12.
+//  Copyright (c) 2012 Rockhopper Technologies. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SimplestGraphTests : SenTestCase
+
+@end
---
+++ SimplestGraphTests/SimplestGraphTests.m
@@ -0,0 +1,32 @@
+//
+//  SimplestGraphTests.m
+//  SimplestGraphTests
+//
+//  Created by Gene De Lisa on 6/26/12.
+//  Copyright (c) 2012 Rockhopper Technologies. All rights reserved.
+//
+
+#import "SimplestGraphTests.h"
+
+@implementation SimplestGraphTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SimplestGraphTests");
+}
+
+@end
